8881e9ff98744236c1ec28ad72124e5866fa799c
carlofabyss <carlofabyss@hotmail.com>
2020-03-16 00:45:34 -0700

    start drawing canvas

---
+++ js/sketch.js
@@ -0,0 +1,13 @@
+let ellipserecord = [];
+
+function setup() {
+    let mycanvas = createCanvas(400, 400);
+}
+
+function draw() {
+    
+}
+
+function mouseDragged() {
+    console.log("X: " + mouseX + ", Y: " + mouseY);
+}
